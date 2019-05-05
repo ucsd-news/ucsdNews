803c42c710307e8dbc541d52cc96c1939f3d758e
Donald Truong <dotruong@ucsd.edu>
2019-05-04 17:24:15 -0700

    Initial Commit

---
+++ UCSDNEWS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UCSDNEWS
+//
+//  Created by Donald Truong on 5/4/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
