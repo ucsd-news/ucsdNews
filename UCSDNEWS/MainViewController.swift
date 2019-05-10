49d90ebe5859e6f4237320302ed7b38e79834973
MajorJon <jtmajor@ucsd.edu>
2019-05-10 03:45:43 -0700

    All segues, passing of information

--- UCSDNEWS/MainViewController.swift
+++ UCSDNEWS/MainViewController.swift
@@ -9,23 +9,38 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var csButton: UIButton!
+    @IBOutlet weak var engineeringButton: UIButton!
+    @IBOutlet weak var cogsciButton: UIButton!
+    @IBOutlet weak var biologyButton: UIButton!
+    @IBOutlet weak var chemButton: UIButton!
+    @IBOutlet weak var musicButton: UIButton!
+    @IBOutlet weak var mathButton: UIButton!
+    @IBOutlet weak var healthButton: UIButton!
+    @IBOutlet weak var humanitiesButton: UIButton!
+    @IBOutlet weak var medButton: UIButton!
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
 
 
         // Do any additional setup after loading the view.
     }
 
-   
     
-    /*
+    
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        let button = sender as! UIButton
+        let keyword = button.titleLabel!.text
+        
+        let newsVC = segue.destination as! newsViewController
+        newsVC.keyword = keyword
     }
-    */
+    
 
 }
